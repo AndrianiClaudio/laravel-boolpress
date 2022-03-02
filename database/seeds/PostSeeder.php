92a6e058fdf7c1286095c24e89908a525db4381f
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-03-02 15:05:12 +0100

    Aggiunta relazione con Category,

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\User;
+use App\Model\Category;
 
 class PostSeeder extends Seeder
 {
@@ -15,17 +16,24 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $ids = [];
-        $users = User::all();
-
-        foreach ($users as $user) {
-            $ids[] = $user->id;
-        }
+        // $ids = [];
+        // $users = User::all();
+        // $categories = [];
+        // $categories = Category::all();
+        // foreach ($users as $user) {
+        //     $ids[] = $user->id;
+        // }
+        // foreach ($categories as $category) {
+        //     $categories[] = $category->id;
+        // }
         // dd($ids[0], $ids[count($ids) - 1]);
         
         for ($i=0; $i < 30; $i++) {
             $newPost = new Post();
-            $newPost->user_id = rand($ids[0], $ids[count($ids) - 1]);
+            // $newPost->user_id = rand($ids[0], $ids[count($ids) - 1]);
+            // $newPost->category_id = rand($categories[0], $categories[count($categories) - 1]);
+            $newPost->user_id = User::inRandomOrder()->first()->id;
+            $newPost->category_id = Category::inRandomOrder()->first()->id;
             $newPost->title = $faker->sentence(3, true);
             $newPost->content = $faker->paragraph(5,true);
             $newPost->slug = Str::slug($newPost->title . '-' . $i);
